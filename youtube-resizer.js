04276193f5b6d8cb08d85c2374b122cc3478e8ac
benftwc <benftwc@gmail.com>
2013-08-27 14:55:41 +0200

    Add licences files, and edit the offset statement for web-browser

--- youtube-resizer.js
+++ youtube-resizer.js
@@ -1,7 +1,17 @@
 /**
-       * Resizing the player in JavaScript.
-       */
-      
+* @author : Benjamin Cassinat <benftwc@gmail.com>
+* https://github.com/benftwc/youtube-full-screen
+*
+* Provide an Youtube fullscreen integration (old method)
+* to allow all browser to use it.
+*
+* Keep in minds : It use the FLASH Youtube Embed Player,
+* and can't be used on iPhone right now, you'll have to
+* change the loadPlayer() method in order to load HTML5 
+* Youtube Player.
+*
+*/
+
       function initYoutubeFullScreen() {
         var myWidth = 0, myHeight = 0;
         if( typeof( window.innerWidth ) == 'number' ) {
@@ -14,14 +24,14 @@
           myHeight = document.documentElement.clientHeight;
         } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
           //IE 4 compatible
-          myWidth = document.body.clientWidth;
-          myHeight = document.body.clientHeight;
+          myWidth = (document.body.clientWidth);
+          myHeight = (document.body.clientHeight);
         }
-        scalePlayer(myWidth, myHeight)
+        scalePlayer(myWidth, myHeight, 40)
       }
 
-      function scalePlayer(w, h) {
-        resizePlayer(w, h);
+      function scalePlayer(w, h, offset) {
+        resizePlayer(w - offset, h - offset);
       }
       // Set the loaded player to a specific height and width.
       function resizePlayer(width, height) {
@@ -30,15 +40,11 @@
         playerObj.width = width;
       }
       
-      // The "main method" of this sample. Called when someone clicks "Run".
       function loadPlayer() {
-        // The video to load
-        var videoID = "ylLzyHk54Z0"
-        // Lets Flash from another domain call JavaScript
+        var videoID = "QC-ma8dV-2w"
         var params = { allowScriptAccess: "always" };
-        // The element id of the Flash embed
         var atts = { id: "ytPlayer" };
-        // All of the magic handled by SWFObject (http://code.google.com/p/swfobject/)
+
         swfobject.embedSWF("http://www.youtube.com/v/" + videoID + 
                            "?version=3&enablejsapi=1&playerapiid=player1", 
                            "videoDiv", "560", "340", "9", null, null, params, atts);
